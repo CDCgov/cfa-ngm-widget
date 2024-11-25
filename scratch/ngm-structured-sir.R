12cf12a0cb9e9b98e2c08ad17e85a368aea4db7f
Paige Miller <yub1@cdc.gov>
2024-11-25 16:33:53 +0000

    testing a function

--- scratch/ngm-structured-sir.R
+++ scratch/ngm-structured-sir.R
@@ -1,7 +1,15 @@
-# script to calculate next generation for 4 group SIR model
+# function to calculate next generation for 4 group SIR model
+ngm_sir_4 <- function(N, V, b_mult, ve, gam) {
+  low <- 0.1
+  high <- b_mult * low
 
-
-ngm_sir_4 <- function(N, V, params) {
+  params <- list(
+    bKK = low, bKC = low, bKG = low, bKT = low,
+    bCK = b_mult, bCC = b_mult, bCG = b_mult, bCT = b_mult,
+    bGK = low, bGC = low, bGG = low, bGT = low,
+    bTK = low, bTC = b_mult, bTG = low, bTT = low,
+    gamma = gam, ve = ve
+  )
   istates <- c("IK", "IC", "IG", "IT")
   flist <- c(
     dIKdt = quote(bKK * SK * IK + bKC * SK * IC + bKG * SK * IG + bKT * SK * IT),
@@ -26,14 +34,10 @@ ngm_sir_4 <- function(N, V, params) {
 
 # inputs
 
-N <- c(.1, .1, .8, 0)
-V <- rep(0, 4) # prop to vaccinate c(.25, .25, .25, .25)
-params <- list(
-  bKK = 1, bKC = 1, bKG = 1, bKT = 1,
-  bCK = 10, bCC = 10, bCG = 10, bCT = 10,
-  bGK = 1, bGC = 1, bGG = 1, bGT = 1,
-  bTK = 1, bTC = 10, bTG = 1, bTT = 1,
-  gamma = 1, ve = 0.7
-)
+N <- c(.100, .100, .800, 0)
+V <- c(0, 0, 0, 0)
+b_mult <- 3
+ve <- 0.7
+gam <- 1 / 7
 
-ngm_sir_4(N, V, params)
+ngm_sir_4(N = N, V = V, b_mult = b_mult, ve = ve, gam = gam)
