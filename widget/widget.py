c5558fea03d04f48c21bc2fd78ca74d2372cbc63
Scott Olesen <ulp7@cdc.gov>
2024-12-06 18:07:23 -0500

    matrix inputs

--- widget/widget.py
+++ widget/widget.py
@@ -7,108 +7,51 @@ import ngm
 
 
 def app():
-    st.title("3-Group NGM Calculator")
-
-    # Information we should be getting from scratch/config.yaml
-    p_severe = np.array([0.02, 0.06, 0.02])
-
-    # Group names
-    group_names = ["Core", "Kids", "General Population"]
-    n_groups = len(group_names)
-    # Sidebar for inputs
-    st.sidebar.header("Model Inputs")
-
-    # Population size
-    st.sidebar.subheader("Population Sizes")
-    default_values = np.array([0.05, 0.45, 0.5]) * int(1e7)
-    N = np.array(
-        [
-        st.sidebar.number_input(f"Population ({group})", value=int(default_values[i]), min_value=0)
-            for i, group in enumerate(group_names)
-        ]
+    # set defaults ------------------------------------------------------------
+    params_default = pd.DataFrame(
+        {
+            "Group name": ["Core", "Children", "Non-core adult"],
+            "Pop. size": np.array([0.05, 0.45, 0.5]) * int(1e7),
+            "No. vaccines": [2.5e5, 2.5e5, 5e5],
+            "Prob. severe": [0.02, 0.06, 0.02],
+        }
     )
 
-    # Vaccine doses
-    st.sidebar.subheader("Vaccine Allocation: Doses")
-    ndoses_default = int(1e6)
-    ndoses = st.sidebar.number_input("Total Number of Doses", value=ndoses_default, min_value=0, max_value=sum(N))
+    n_groups_default = params_default.shape[0]
 
-    st.sidebar.subheader("Vaccine Allocation: Strategies")
-    strategy = st.sidebar.selectbox(
-        "Vaccine allocation strategy",
-        [
-            "Core", "Kids", "Even",
-        ]
+    K_default = pd.DataFrame(
+        {"to": params_default["Group name"]}
+        | {f"from {x}": [0.0] * n_groups_default for x in params_default["Group name"]}
+    )
+    K_default.set_index("to")
+    K_default.iloc[:, 1:] = np.array(
+        [[3.0, 0.0, 0.2], [0.1, 1.0, 0.5], [0.25, 1.0, 1.5]]
     )
 
-    button_to_core = {"Core" : 0, "Kids" : 1, "Even" : "even"}
+    st.title("NGM Calculator")
 
-    allocation_default = [0] * n_groups
-    if ndoses > 0:
-        allocation_default = ngm.distribute_vaccines(V=ndoses, N_i=N, strategy=button_to_core[strategy])
-        allocation_default = 100 * allocation_default / allocation_default.sum()
+    st.header("Model inputs")
 
-    st.sidebar.subheader("Vaccine Allocation: Customization")
-    allocation = []
-    remaining = 100.0
-    for i, group in enumerate(group_names[:-1]):
-        this_max = 100.0 - sum(allocation[:i])
-        if (this_max / 100.0) * ndoses > N[i]:
-            this_max = N[i] / ndoses * 100
+    params = st.data_editor(params_default)
 
-        allocation.append(
-            st.sidebar.number_input(
-                f"Percent of Vaccine Doses going to {group}",
-                value=allocation_default[i],
-                min_value=0.0,
-                max_value=this_max,
-                step=1.0
-            )
-        )
-        remaining -= allocation[-1]
-    st.sidebar.write(f"(Allocating {remaining:.2f}% to {group_names[-1]})")
-    allocation.append(remaining)
+    K_df = st.data_editor(K_default, disabled=["to"], hide_index=True)
+    K = K_df.iloc[:, 1:].to_numpy()
 
-    V = np.floor(np.array(allocation) / 100.0 * ndoses).astype("int")
-
-    # Contact matrix
-    st.sidebar.subheader("Next Generation Matrix")
-    st.sidebar.write("For a single new infection of category `from`, specify how many infections of category `to` it will make.")
-
-    from_to = [((i, group_names[i]), (j, group_names[j]),) for i in range(n_groups) for j in range(n_groups)]
-    r_default = np.array([[3.0, 0.0, 0.2], [0.10, 1.0, 0.5], [0.25, 1.0, 1.5]])
-
-    r_novax = np.zeros((n_groups, n_groups,))
-    for ft in from_to:
-        row = ft[1][0]
-        col = ft[0][0]
-        r_novax[row, col] = st.sidebar.number_input(
-            f"From {ft[0][1]} to {ft[1][1]}",
-            value=r_default[row, col],
-            min_value=0.0,
-            max_value=100.0
-        )
-
-    with st.sidebar.expander("Advanced Settings"):
-        st.sidebar.subheader("Vaccine efficacy")
-        VE = st.sidebar.slider("Vaccine Efficacy", 0.0, 1.0, value=0.7, step=0.01)
+    VE = st.slider("Vaccine Efficacy", 0.0, 1.0, value=0.7, step=0.01)
 
     # Perform the NGM calculation
     result = ngm.simulate(
-        R_novax=r_novax, n=N, n_vax=V, p_severe=p_severe, ve=VE
+        R_novax=K,
+        n=params["Pop. size"].to_numpy(),
+        n_vax=params["No. vaccines"].to_numpy(),
+        p_severe=params["Prob. severe"].to_numpy(),
+        ve=VE,
     )
 
-    # Display the adjusted contact matrix
-    st.subheader("Results with vaccination:")
-
-    R = pd.DataFrame(
-        np.round(result["R"], 2),
-        columns=group_names,
-        index=group_names,
-    )
+    st.header("Results with vaccination")
 
     st.write("Next Generation Matrix:")
-    st.dataframe(R)
+    st.dataframe(result["R"])
 
     # Display results
     st.write(f"R-effective: {result['Re']:.2f}")
@@ -116,30 +59,28 @@ def app():
     st.dataframe(
         pd.DataFrame(
             [np.round(result["infections"], 2)],
-            columns=group_names,
+            columns=params["Group name"],
         ).style.hide(axis="index")
     )
-    st.write(f"Infection fatality ratio: {(p_severe * result['infections']).sum():.3f}")
+    st.write(
+        f"Infection fatality ratio: {(params['Prob. severe'].to_numpy() * result['infections']).sum():.3f}"
+    )
 
     st.subheader("Counterfactual (no vaccination):")
     st.write("Next Generation Matrix:")
-    st.dataframe(
-        pd.DataFrame(
-            np.round(r_novax, 2),
-            columns=group_names,
-            index=group_names,
-        )
-    )
-    novax_eigen = ngm.dominant_eigen(r_novax)
+
+    novax_eigen = ngm.dominant_eigen(K)
     st.write(f"R0: {novax_eigen.value:.2f}")
     st.write("Proportion of infections in each group:")
     st.dataframe(
         pd.DataFrame(
             [np.round(novax_eigen.vector, 2)],
-            columns=group_names,
+            columns=params["Group name"],
         ).style.hide(axis="index")
     )
-    st.write(f"Infection fatality ratio: {(p_severe * novax_eigen.vector).sum():.3f}")
+    st.write(
+        f"Infection fatality ratio: {(params['Prob. severe'].to_numpy() * novax_eigen.vector).sum():.3f}"
+    )
 
 
 if __name__ == "__main__":
